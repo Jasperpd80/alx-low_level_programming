695e17ff907632062a3cf6a329fc295efbd24b84
daniel <danielpaulayock@gmail.com>
2022-07-05 20:44:46 -0700

    nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
  * print sign - function that prints the sign of a number
  * @n:int
- * Return: Return 1 and prints + if n is greater than zero
- * 	  Returns 0 and prints 0 if n is zero
- * 	  Returns -1 and prints - if n is less than zero
+ * Return:	Return 1 and prints + if n is greater than zero
+ * 		Returns 0 and prints 0 if n is zer
+ * 		Returns -1 and prints - if n is less than zero
  */
 
 int print_sign(int n)
