3c57121131d6b63780d38e1d06c53b7b3beaf29e
daniel <danielpaulayock@gmail.com>
2022-07-05 20:15:54 -0700

    nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,14 +12,11 @@ int _islower(int c)
 
 {
 	if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
 
-					return (1);
-
-			else
-				return (0);
-
-				/* putchar (c); */
-
-				/* printf("la va de c=%d\n", c); */
+	/* putchar (c); */
+	/* printf("la va de c=%d\n", c); */
 
 }
