ec1466bd08b5f174b37f7f667666164759170dc6
daniel <danielpaulayock@gmail.com>
2022-07-26 22:24:03 -0700

    Malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -72,8 +72,9 @@ unsigned int nbr_spaces(char *s)
 char **strtow(char *str)
 {
 	int i;
-	int spaces = nbr_spaces(str);
-	char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
+	/*int spaces = nbr_spaces(str);*/
+	char **tokens = NULL;
+	/*malloc(sizeof(char *) * (spaces));*/
 	char *token;
 	int checkingSpace = 0;
 	int word = 0;
