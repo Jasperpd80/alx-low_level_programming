d80cf1ac7c4350e6cd388a8ff758311791fecf55
daniel <danielpaulayock@gmail.com>
2022-08-01 06:43:29 -0700

    Structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
-/*int get_len(int i);
-char *str_cpy(char *dest, char *src);*/
+
 /**
  * new_dog - a function that creates a new dog
  * get len of name + owner, malloc them, cpy name + owner to new
@@ -11,6 +10,7 @@ char *str_cpy(char *dest, char *src);*/
  * @owner: owner
  * Return: 0
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
