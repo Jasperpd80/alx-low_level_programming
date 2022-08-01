72ee3a34a97e77acca7cc13f31a88dad462e9d06
daniel <danielpaulayock@gmail.com>
2022-08-01 06:46:26 -0700

    Structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,7 +19,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = name;
-	new_dog->owner = owner;
 	new_dog->age = age;
+	new_dog->owner = owner;
 	return (new_dog);
 }
