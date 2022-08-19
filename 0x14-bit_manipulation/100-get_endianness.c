30701dd5a9c36df1b9959f7401c7b80898404c5f
Jasperpd80 <danielpaulayock@gmail.com>
2022-08-18 21:52:48 -0700

    bit additional task

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -23,10 +23,10 @@
 int get_endianness(void)
 
 {
-	int num = 1;
-	char *endian = (char *)&num;
-	
-	if (*endian == 1)
-		return (1);
-	return (0);
+	int x;
+	char *y;
+
+	x = 1;
+	y = (char *)&x;
+	return (*y);
 }
