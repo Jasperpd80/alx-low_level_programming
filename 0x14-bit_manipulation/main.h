f260e9f8c45862647c7e6db533410d197af2bc4c
Jasperpd80 <danielpaulayock@gmail.com>
2022-08-18 20:30:59 -0700

    bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #define BYTE_LENGHT 8
 
@@ -12,6 +12,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-
+int check_valid_string(const char *b);
 
 #endif
